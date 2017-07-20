17000e12fff29ece35853fea0b57febaa961df46
Unknown <calebereid@gmail.com>
2017-07-20 14:45:20 -0500

    final

--- app/src/main/java/com/creid/finalapp/MainActivity.java
+++ app/src/main/java/com/creid/finalapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.creid.finalapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +22,22 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
-        public void gotoManage(View view){
+        public void goToManage(View view){
 
-            //test
-       }
+
+        //Create an intent in order to launch the Manage Activity
+        Intent intent = new Intent(getApplicationContext(), ManageActivity.class);
+
+        startActivity(intent);
+    }
+
+
+
 
        public void tryLogin(View view) {
+
+           SharedPreferences sharedPreferences = this.getSharedPreferences("com.creid.sharedpreferences", Context.MODE_PRIVATE);
+
 
            EditText usernameEditText = (EditText) findViewById(R.id.EmailText);
            EditText passwordEditText = (EditText) findViewById(R.id.PasswordText);
@@ -33,9 +45,16 @@ public class MainActivity extends AppCompatActivity {
            String username = usernameEditText.getText().toString();
            String passwordEntered = passwordEditText.getText().toString();
 
-           Log.i("Info", "Username is:" + username);
-           Log.i("Info", "Username is:" + username);
+           String passwordStored = sharedPreferences.getString(username, "");
 
+           Log.i("Info", "Username is:" + username);
+           Log.i("Info", "Password entered is:" + passwordEntered);
+           Log.i("Info", "Password stored is:" + passwordStored);
+
+           if (passwordStored.equals(passwordEntered)) {
+               Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+               goToManage(view);
+           }
        }
 
     
